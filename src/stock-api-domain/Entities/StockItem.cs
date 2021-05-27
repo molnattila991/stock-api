b1643442fe9ff454d9bee2ace8a2e2e9cb559d51
molnattila991 <molnarattila991@gmail.com>
2021-05-27 12:04:58 +0200

    -(add) stockItem entity

---
+++ src/stock-api-domain/Entities/StockItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stock_api_domain.Entities
+{
+    class StockItem
+    {
+        public string Type { get; set; }
+        public int ValueOfType { get; set; }
+        public int Amount { get; set; }
+    }
+}
