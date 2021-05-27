5102d920658021d350d5d2ff246a467e74a7305c
molnattila991 <molnarattila991@gmail.com>
2021-05-27 15:33:58 +0200

    -(add) checkOut controller, command, dto

---
+++ src/stock-api-application/Features/CheckOut/Commands/CheckOutStockCommand.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using stock_api_domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace stock_api_application.Features.CheckOut.Commands
+{
+    public class CheckOutStockCommand : IRequest<IEnumerable<StockItem>>
+    {
+        public IEnumerable<StockItem> items { get; set; }
+        public int Price { get; set; }
+    }
+
+    public class CheckOutStockCommandHandler : IRequestHandler<CheckOutStockCommand, IEnumerable<StockItem>>
+    {
+        public Task<IEnumerable<StockItem>> Handle(CheckOutStockCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/stock-api/Controllers/v1/CheckOutController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using stock_api.DTOs.Stock;
+using stock_api_application.Features.CheckOut.Commands;
+using stock_api_domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace stock_api.Controllers.v1
+{
+    [ApiVersion("1.0")]
+
+    public class CheckOutController : BaseApiController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post(CheckOutStockRequest request)
+        {
+            var command = new CheckOutStockCommand()
+            {
+                items = request.Inserted.Select(item => new StockItem()
+                {
+                    Amount = item.Value,
+                    Type = item.Key,
+                    ValueOfType = Convert.ToInt32(item.Key)
+                    //TODO if key is not number, throw exception
+                }),
+                Price = request.Price
+            };
+
+            var result = await Mediator.Send(command);
+            return Ok(result);
+        }
+    }
+}
