d7c3496eb9f3b1f29ffa4af8eb94a8636786bd21
molnattila991 <molnarattila991@gmail.com>
2021-05-27 13:42:23 +0200

    -(add) base controller, stock controller -(add) api versioning -(add) dtos

---
+++ src/stock-api/Controllers/v1/StockController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using stock_api.DTOs.Stock;
+using stock_api_application.Features.Stock.Commands;
+using stock_api_domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace stock_api.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    public class StockController : BaseApiController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateStockRequest request)
+        {
+            var command = new StockCommand()
+            {
+                items = request.Inserted.Select(item => new StockItem()
+                {
+                    Amount = item.Value,
+                    Type = item.Key,
+                    ValueOfType = Convert.ToInt32(item.Key)
+                })
+            };
+            return Ok(await Mediator.Send(command));
+        }
+    }
+}
---
+++ src/stock-api/DTOs/Stock/CreateStockRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace stock_api.DTOs.Stock
+{
+    public class CreateStockRequest
+    {
+        public Dictionary<string, int> Inserted { get; set; }
+    }
+}
