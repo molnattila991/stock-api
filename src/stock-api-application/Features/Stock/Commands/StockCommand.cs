4d800f858ac65abc556eba4fa5ba3b083d7dc509
molnattila991 <molnarattila991@gmail.com>
2021-05-27 13:23:05 +0200

    -(add) mediatr

---
+++ src/stock-api-application/Features/Stock/Commands/StockCommand.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using stock_api_application.Interfaces;
+using stock_api_domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace stock_api_application.Features.Stock.Commands
+{
+    public class StockCommand : IRequest<bool>
+    {
+        public IEnumerable<StockItem> items { get; set; }
+    }
+
+    public class StockCommandHandler : IRequestHandler<StockCommand, bool>
+    {
+        private readonly IStockRepository _stockRepository;
+
+        public StockCommandHandler(IStockRepository stockRepository)
+        {
+            _stockRepository = stockRepository;
+        }
+
+        public async Task<bool> Handle(StockCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await _stockRepository.AddItems(request.items);
+            }
+            catch (Exception e)
+            {
+                //TODO log
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ src/stock-api-application/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace stock_api_application
+{
+    public static class ServiceExtensions
+    {
+        public static void AddApplicationLayer(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+        }
+    }
+}
